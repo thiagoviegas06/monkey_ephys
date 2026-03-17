ce5827e7661ee25643406c165d9944010c8349f3
Diego <diegorosenberg@gmail.com>
2026-03-17 15:56:26 -0400

    Reset config.py to HPC settings and not local

--- src_mae/config.py
+++ src_mae/config.py
@@ -24,10 +24,10 @@ class Config:
     kin_channels = 4
     
     # For transformer model
-    d_model = 8  # Embedding dimension for transformer
-    nhead = 2  # Number of attention heads
+    d_model = 64  # Embedding dimension for transformer
+    nhead = 8  # Number of attention heads
     num_layers = 6 # Number of transformer encoder blocks stacked on top of each other
-    tcn_levels = 2  # Number of TCN dilation layers
+    tcn_levels = 8  # Number of TCN dilation layers
     dropout = 0.1  # Dropout rate in transformer for regularization
     
     # Training
